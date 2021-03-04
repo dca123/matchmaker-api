67d1ef978bb1b8b2903285b02d91f2b7d2d27059
Devinda Senanayaka <dca123@users.noreply.github.com>
2021-03-04 16:01:39 -0600

    feat(searchqueue.ts): changed lobbysize to 2

--- src/libs/SearchQueue.ts
+++ src/libs/SearchQueue.ts
@@ -33,7 +33,7 @@ export default class SearchQueue {
     playerMap: Map<string, Player>
   ): [Lobby, Ticket[]] | [false] {
     // Meet conditions
-    if (this.queue.length >= 1) {
+    if (this.queue.length >= 2) {
       const lobbyTickets = this.dequeue();
       const newLobby = new Lobby(lobbyTickets, playerMap);
       return [newLobby, lobbyTickets];
