51fa7929cc5be4bb665f1987cef1a454bbe684ab
Devinda Senanayaka <dca123@users.noreply.github.com>
2021-03-06 01:32:13 -0600

    ci(loaders/logger.ts): added pino logger

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 import dota2 from 'dota2';
 
-const lobbyConfig = {
+export const lobbyConfig = {
   game_name: 'node-dota2',
   server_region: dota2.ServerRegion.USWEST,
   game_mode: dota2.schema.DOTA_GameMode.DOTA_GAMEMODE_AP,
@@ -14,5 +14,12 @@ const lobbyConfig = {
   dire_series_wins: 0,
   allchat: true,
 };
-
-export default lobbyConfig;
+export const pinoSettings = {
+  level: process.env.LOG_LEVEL || 'trace',
+  prettyPrint: {
+    colorize: true,
+    ignore: 'pid,hostname',
+    levelFirst: true,
+    translateTime: 'yyyy-mm-dd HH:MM:ss.l',
+  },
+};
